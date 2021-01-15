d3961b99f2176fceccaf580409775617320bc611
edodema <edodema13@gmail.com>
2021-01-15 06:17:19 +0100

    Another bug

--- src/utils/refine_dataset.py
+++ src/utils/refine_dataset.py
@@ -30,15 +30,21 @@ class RefineDataset(Utils):
             for bboxes_file in os.listdir(bboxes_dir):
                 bboxes_file_path = os.path.join(bboxes_dir, bboxes_file)
                 # The frame number is took directly from the bbox file's name
-                frame = int(re.sub(r"\..*", '', bboxes_file)) 
-                
-                valence = valences[frame]
+                frame = int(re.sub(r"\..*", '', bboxes_file))
 
-                if not( -0.1 <= valence and valence <= 0.1): # to avoid  only noise
-                    bboxes = self.get_bboxes(bboxes_file_path)
-                    rectangles = [ [tuple(bbox[0]), tuple(bbox[2])] for bbox in bboxes ]
+                """ 
+                NOTE:
+                Strangely this is a problem, don't know why there is 
+                one bbox exceeding. I ignore it
+                """ 
+                if frame < len(valences): 
+                    valence = valences[frame]
+
+                    if not( -0.1 <= valence and valence <= 0.1): # to avoid  only noise
+                        bboxes = self.get_bboxes(bboxes_file_path)
+                        rectangles = [ [tuple(bbox[0]), tuple(bbox[2])] for bbox in bboxes ]
                     
-                    arr += [[file, frame, rectangles, valence]]
+                        arr += [[file, frame, rectangles, valence]]
 
         df = pd.DataFrame(np.array(arr, dtype=object), columns=['video', 'frame', 'faces', 'valence'])
         df.to_csv(csv_path)
