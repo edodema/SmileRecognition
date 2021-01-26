100f8bb064ca2acd7b0ab1118f3be57b419b5ea3
edodema <edodema13@gmail.com>
2021-01-26 04:30:26 +0100

    Moved

--- examples/temp.py
+++ examples/temp.py
@@ -12,18 +12,8 @@ from sklearn import svm
 from src.detection.violajones import ViolaJones
 from src.detection.landmark import Landmark
 from src.utils.utils import Utils
-from src.recognition.recognition import Recognition
+from src.recognition.svm import SVM
 from src.test.test import Test
-
-def plot_roc_cur(fper, tper):  
-    plt.plot(fper, tper, color='orange', label='ROC')
-    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend()
-    plt.show()
-
 
 video = 'datasets/affwild/videos/train/318.mp4'
 files_path='datasets/affwild/videos/train' 
@@ -36,7 +26,7 @@ svm_path = 'datasets/svm_complete.yml'
 landmark = Landmark()
 violajones = ViolaJones()
 ut = Utils()
-recognition = Recognition()
+recognition = SVM('skl')
 test = Test(landmarks_path, valences_path)
 
 X_train, X_test, y_train, y_test = test.split_dataset()
@@ -46,23 +36,29 @@ print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
+"""
 clf = svm.SVC()
 clf.fit(X_train, y_train)
 prediction = clf.predict(X_test)
-"""
+
 svm = cv2.ml.SVM_create()
 svm.setType(cv2.ml.SVM_C_SVC)
-svm.setKernel(cv2.ml.SVM_POLY)
+svm.setKernel(cv2.ml.SVM_RBF)
 svm.setDegree(3)
+svm.setC(1)
+svm.setGamma(0.00666666666667)
 svm.setTermCriteria((cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6))
 svm.train(X_train, cv2.ml.ROW_SAMPLE, y_train)
 
-test = np.arange(64).astype(np.float32).reshape(1,64)
-
+#test_prediction = np.arange(64).astype(np.float32).reshape(1,64)
 prediction = svm.predict(X_test)[1].flatten().astype(int)
 """
 
+recognition.train_svm(landmarks_path, valences_path, 'datasets/svm_complete_skl.yml')
+
+"""
 fper, tper, thresholds = metrics.roc_curve(y_test, prediction) 
 auc = metrics.auc(fper, tper)
 print(auc)
-plot_roc_cur(fper, tper)
+test.plot_roc(fper, tper)
+"""
